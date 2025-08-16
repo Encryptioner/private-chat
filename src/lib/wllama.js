82a02d981cbd388fd6cbfdcdd018c8df5bb73dc1
nadchif <47924887+nadchif@users.noreply.github.com>
2025-08-16 19:30:44 +0800

    add Gemma3 270M

--- src/lib/wllama.js
+++ src/lib/wllama.js
@@ -14,40 +14,45 @@ export const CHAT_ROLE = Object.freeze({
 
 const isLocalHost = ["localhost", "0.0.0.0", "127.0.0.1"].includes(window.location.hostname);
 
-export const PRESET_MODELS = {
-  "Gemma 3 (1B)": {
-    name: "Gemma 3 (1B)",
+const models = {
+  "Gemma 3 (270M)": {
     url: isLocalHost
-      ? `${window.location.origin}/models/gemma-3-1b-it-Q4_K_M.gguf`
-      : "https://huggingface.co/unsloth/gemma-3-1b-it-GGUF/resolve/main/gemma-3-1b-it-Q4_K_M.gguf",
+      ? `${window.location.origin}/models/gemma-3-270m-it-Q8_0.gguf`
+      : "https://huggingface.co/unsloth/gemma-3-270m-it-GGUF/resolve/main/gemma-3-270m-it-Q8_0.gguf",
     license: "https://deepmind.google/models/gemma/gemma-3",
     description: "Gemma is a lightweight, family of models from Google built on Gemini technology.",
   },
+  "SmolLM2 (360M)": {
+    url: "https://huggingface.co/unsloth/SmolLM2-360M-Instruct-GGUF/resolve/main/SmolLM2-360M-Instruct-Q4_K_M.gguf",
+    license: "https://huggingface.co/HuggingFaceTB/SmolLM2-360M##license",
+    description: "SmolLM2 is a family of compact language models",
+  },
   "Qwen 3 (0.6B)": {
-    name: "Qwen 3 (0.6B)",
     url: "https://huggingface.co/unsloth/Qwen3-0.6B-GGUF/resolve/main/Qwen3-0.6B-Q4_K_M.gguf",
     license: "https://qwenlm.github.io/",
     description: "The 0.6B version of Qwen 3",
   },
   "Llama 3.2 (1B)": {
-    name: "Llama 3.2 (1B)",
     url: "https://huggingface.co/hugging-quants/Llama-3.2-1B-Instruct-Q4_K_M-GGUF/resolve/main/llama-3.2-1b-instruct-q4_k_m.gguf",
     license: "https://raw.githubusercontent.com/meta-llama/llama-models/refs/heads/main/models/llama3_2/LICENSE",
     description: "The 1B version of The Meta Llama 3.2 collection.",
   },
-  "Falcon3 (1B)": {
-    name: "Falcon3 (1B)",
-    url: "https://huggingface.co/tiiuae/Falcon3-1B-Instruct-GGUF/resolve/main/Falcon3-1B-Instruct-q4_k_m.gguf",
-    license: "https://falconllm.tii.ae/terms-and-conditions.html",
-    description: "The 1B version of Falcon3 family of Open Foundation Models",
-  },
   "Deepseek R1 (1.5B)": {
-    name: "Deepseek R1 (1.5B)",
     url: "https://huggingface.co/bartowski/DeepSeek-R1-Distill-Qwen-1.5B-GGUF/resolve/main/DeepSeek-R1-Distill-Qwen-1.5B-Q4_K_M.gguf",
     license: "https://huggingface.co/deepseek-ai/DeepSeek-R1#7-license",
     description: "The 1.5B version of Deepseek R1",
   },
 };
+
+export const PRESET_MODELS = Object.fromEntries(
+  Object.entries(models).map(([name, rest]) => [
+    name,
+    {
+      name,
+      ...rest,
+    },
+  ])
+);
 
 export const formatChat = async (wllamaInstance, messages) => {
   const template = new Template(wllamaInstance.getChatTemplate() ?? CHAT_TEMPLATE);
